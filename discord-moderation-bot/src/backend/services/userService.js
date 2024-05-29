94a23417219e60881da1d1c8fd63e4214789e732
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 20:27:25 +0530

    Added: `discord-moderation-bot/src/backend/services/userService.js`

---
+++ discord-moderation-bot/src/backend/services/userService.js
@@ -0,0 +1,16 @@
+const userService = {
+  getUsers: () => {
+    // Logic to fetch users from database
+  },
+  getUserById: (userId) => {
+    // Logic to fetch user by userId from database
+  },
+  updateUser: (userId, userData) => {
+    // Logic to update user data in the database
+  },
+  deleteUser: (userId) => {
+    // Logic to delete user from the database
+  },
+};
+
+module.exports = userService;
